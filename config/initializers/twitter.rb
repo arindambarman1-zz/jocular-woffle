3b8ce935fdf64607fbb1a5bbeede93cd61c2aebb
Arindam Barman <1arindambarman@gmail.com>
2015-09-15 00:28:53 +0530

    first commit

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+TWITTER = Twitter::REST::Client.new do |config|
+  config.consumer_key        = 'qc38gGeZir6cLh4ljsgegA3D9'
+  config.consumer_secret     = 'bmPfX8kN1fYoKw7E7UTnbml1WYq3m64r1N0XIv0hlDt5x56qPx'
+  config.access_token        = '61428197-MFTLYuQVdSphCevOvfenQ4J0epTlxo8zVVLrIYF8J'
+  config.access_token_secret = 'lkXp1KtWPeyixuHqo5WS5mc0zdPb87c48urhGV5H3ggc4'
+end
