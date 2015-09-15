65e77a55a378bb6ff93113309334bc0c1dd43135
Swapnil Debarshi <swapnil.debarshi@gmail.com>
2015-09-16 01:28:37 +0530

    Handle HTTP Error

--- lib/function.rb
+++ lib/function.rb
@@ -11,34 +11,23 @@ class Function
 		coords = grid.create_grid(min_lat, max_lat, min_long, max_long)
 		payload = {'type': 'place', 'distance': '3000', 'limit': 500}
 		payload_business = {"fields": "checkins,were_here_count"}
-		@i = 0
 		j = 0
 		coords.each do |coord|
-			print("Trying to Gather places for Coordinate Pair #{j+1}\n")
-			payload['center'] = coord.join(",")
-			feed = FB.graph_call('search',args=payload)
-			# Check for empty next_page
-			k = 0
-			while feed.size != 0 do
-				feed.each do |f|
-					business_id = f["id"]
-					business_name = f["name"]
-					cat_name = f["category_list"][0]["name"]
-					latitude = f["location"]["latitude"]
-					longitude = f["location"]["longitude"]
-
-					@place = Place.new(business_id: business_id, business_name: business_name, cat_name: cat_name,
-					latitude: latitude, longitude: longitude, city_id: city_id)
-					@place.save
-					@i += 1
-					k += 1
-				end
-				feed = feed.next_page
+			begin
+				print("Trying to Gather places for Coordinate Pair #{j+1} for #{city['name']}\n")
+				payload['center'] = coord.join(",")
+				feed = FB.graph_call('search',args=payload)
+				enter_into_db(feed, city_id)
+			rescue Faraday::ConnectionFailed
+				sleep 5
+				print "Error Handling for Coordinate Pair #{j+1}\n"
+				feed = FB.graph_call('search',args=payload)
+				enter_into_db(feed, city_id)
 			end
-			print("Found #{k} places for Coordinate Pair #{j+1}\n")
 			j += 1
 		end
 	end
+
 
 	
 	def get_all_places
@@ -46,7 +35,29 @@ class Function
 			get_grid(city["name"])
 		end
 	end
+
+
+	def enter_into_db(feed, city_id)
+		k = 0
+		while feed.size != 0 do
+			feed.each do |f|
+				business_id = f["id"]
+				business_name = f["name"]
+				cat_name = f["category_list"][0]["name"]
+				latitude = f["location"]["latitude"]
+				longitude = f["location"]["longitude"]
+
+				@place = Place.new(business_id: business_id, business_name: business_name, cat_name: cat_name,
+				latitude: latitude, longitude: longitude, city_id: city_id)
+				@place.save
+				k += 1
+			end
+			feed = feed.next_page
+		end
+		print("Found #{k} places for Coordinate Pair\n")
+	end
 end
+
 
 __END__
 Try for batch ** something like this?? **
