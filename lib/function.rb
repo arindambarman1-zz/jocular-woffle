69ffff45fa163bec290cbcf347c158eb1d32222a
Swapnil Debarshi <swapnil.debarshi@gmail.com>
2015-09-15 20:38:57 +0530

    Add Grid

--- lib/function.rb
+++ lib/function.rb
@@ -1,39 +1,63 @@
-require "#{Rails.root}/app/models/place"
-
 class Function
 
-	# Changed into a instance method
-	# Bangalore = 12.9667, 77.5667
-	def get_place(lat,long)
+	def get_grid()
+		grid = Grid.new
+		min_lat = 12.773509
+		max_lat = 13.162649
+		min_long = 77.391904
+		max_long = 77.803226
+		coords = grid.create_grid(min_lat, max_lat, min_long, max_long)
 		@i = 0
-		@coord = [lat,long].join(",")
-		payload = {'type': 'place', 'center': @coord, 'distance': '3000', 'limit': 10}
+		payload = {'type': 'place', 'distance': '3000', 'limit': 500}
 		payload_business = {"fields": "checkins,were_here_count"}
+		j = 0
+		coords.each do |coord|
+			print("Trying to Gather places for Coordinate Pair #{j+1}\n")
+			payload['center'] = coord.join(",")
+			feed = FB.graph_call('search',args=payload)
+			# Check for empty next_page
+			k = 0
+			while feed.size != 0 do
+				feed.each do |f|
+					business_id = f["id"]
+					business_name = f["name"]
+					cat_name = f["category_list"][0]["name"]
+					latitude = f["location"]["latitude"]
+					longitude = f["location"]["longitude"]
 
-		feed = FB.graph_call('search',args=payload)
-
-		# Check for empty next_page
-		while feed.size != 0 do
-			feed.each do |f|
-				business_id = f["id"]
-				business_name = f["name"]
-				cat_name = f["category_list"][0]["name"]
-				latitude = f["location"]["latitude"]
-				longitude = f["location"]["longitude"]
-
-				# Make another call to collect object details
-				details = FB.get_object(f['id'], args = payload_business)
-				
-				checkins = details["checkins"]
-
-				@place = Place.new(business_id: business_id, business_name: business_name, cat_name: cat_name,
+					# Make another call to collect object details
+					details = FB.get_object(f['id'], args = payload_business)
+					
+					checkins = details["checkins"]
+					@place = Place.new(business_id: business_id, business_name: business_name, cat_name: cat_name,
 					latitude: latitude, longitude: longitude, checkins: checkins)
-				@place.save
-				@i += 1
+					@place.save
+					@i += 1
+					k += 1
+				end
+				feed = feed.next_page
 			end
-
-			feed = feed.next_page
+			print("Found #{k} places for Coordinate Pair #{j+1}\n")
+			j += 1
 		end
-		print ("Inserted #@i places")
+		print ("Found #@i places in Bangalore\n")
 	end
 end
+
+__END__
+Try for batch ** something like this?? **
+feed = FB.batch do |batch_api|
+				batch_api.graph_call('search',args=payload)
+				while feed.each.size != 0 do
+					feed.each do |f|
+						business_id = f["id"]
+						business_name = f["name"]
+						cat_name = f["category_list"][0]["name"]
+						latitude = f["location"]["latitude"]
+						longitude = f["location"]["longitude"]
+
+						details = FB.get_object(f['id'], args = payload_business)
+				
+						checkins = details["checkins"]
+					end
+					feed = feed.next_page
